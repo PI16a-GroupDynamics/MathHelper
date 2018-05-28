9bf578859c986cabb422fcffd22f770b1d324dc2
Svetlana Berdiukova <37275290+Lannica@users.noreply.github.com>
2018-05-28 10:08:38 +0300

    update v.0.1.2

---
+++ mathhelper v.0.1.2/mathhelper/Form2.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mathhelper
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+        }
+
+        private double FirstFrm(int n, double res) //вычисления по первой формуле
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    res -= Math.Sqrt(i);
+                }
+                else if (i == n)
+                {
+                    res += Math.Sqrt(i);
+                }
+                else res += i;
+            }
+            return res;
+        }
+
+        private double SecondFrm(int n, double res) //вычисления по вторрой формуле
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                res += Math.Pow(i, i);
+            }
+            return res;
+        }
+
+        private double ThirdFrm(int n, double res) //вычисления по третьей формуле
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                res += 1 / i;
+            }
+            return res;
+        }
+
+        private void button1_Click(object sender, EventArgs e) //кнопка "вычислить"
+        {
+            richTextBox1.Clear();
+            double res = 0;
+            int n;
+            int rnd;
+            
+            n = Convert.ToInt32(textBox1.Text);
+            rnd = Convert.ToInt32(textBox2.Text);
+            if (comboBox1.SelectedIndex == 1) res = FirstFrm(n, res);
+            else if (comboBox1.SelectedIndex == 2) res = SecondFrm(n, res);
+            else if (comboBox1.SelectedIndex == 3) res = ThirdFrm(n, res);
+            else
+            {
+                //round округляет только для 15 знаков!
+                res = Math.Round(FirstFrm(n, res), rnd);
+                richTextBox1.AppendText(Convert.ToString(res) + Environment.NewLine);
+                res = Math.Round(SecondFrm(n, res), rnd);
+                richTextBox1.AppendText(Convert.ToString(res) + Environment.NewLine);
+                res = Math.Round(ThirdFrm(n, res), rnd);
+                richTextBox1.AppendText(Convert.ToString(res) + Environment.NewLine);
+            }
+        }
+
+       
+
+        private void справкаToolStripMenuItem_Click(object sender, EventArgs e) //вызов справки по проекту
+        {
+            Help help = new Help();
+            help.Show();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e) //контроль ввода данных для n
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8) // цифры и клавиша BackSpace
+            {
+                e.Handled = true;
+            }
+           
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e) //контроль ввода данных для числа округления
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8) // цифры и клавиша BackSpace
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ mathhelper v.0.1.2/mathhelper/Help.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace mathhelper
+{
+    partial class Help
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Справка по программе MathHelper";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Версия программы: 0.1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(325, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Данная программа призвана облегчить работу математиков, \r\nпроизводя вычисления по" +
+    " формулам";
+            // 
+            // Help
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 113);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Help";
+            this.Text = "Help";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
